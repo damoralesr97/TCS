bac7ec3c7dadc4a4296ee301f5972ad021e8e775
David Morales <damoralesr97@outlook.com>
2024-09-10 22:20:50 -0500

    Crear modelos de Persona y Cliente

---
+++ cliente-persona/src/main/java/com/morales/cliente_persona/utils/enums/GenreEnum.java
@@ -0,0 +1,13 @@
+package com.morales.cliente_persona.utils.enums;
+
+public enum GenreEnum {
+
+    M("masculino"),
+    F("femenino");
+
+    private String genre;
+
+    GenreEnum(String genre) {
+        this.genre = genre;
+    }
+}
